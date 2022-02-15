fb0843f5a9abae30f475c3decad760f4cd3ff587
Ahmed Omar <ahmedomar@hey.com>
2022-02-15 15:53:11 +0000

    added some JS variables and functions

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // variables
-const inputButtons = document.getElementsByClassName("input-buttons");
+const numButton = document.getElementsByClassName("num-button");
+const operatorButton = document.getElementsByClassName("operator-button");
 const display = getElementById("result");
+const clear = getElementById("AC");
+
+
+
 // functions:
 
 const onInputButtonClick = (event) => {
@@ -13,20 +18,27 @@ const displayFunc = () => {
   document.getElementById("result").value;
   alert("it's working");
 };
+
+
 //Evaluate and give result
 const solve = () => {
   let x = document.getElementById("result").value;
   let y = document.getElementById("result").value;
 };
 
+
 //To clear the display
-const clr = () => {
+const clearAll = () => {
   document.getElementById("result").value = "";
 };
 
+
+
 // logic
-for (let index = 0; index < inputButtons.length; index++) {
-  inputButtons[index].addEventListener("click", displayFunc);
+for (let index = 0; index < numButton.length; index++) {
+  numButton[index].addEventListener("click", displayFunc);
 }
 
+operatorButton.addEventListener("click", solve);
 
+clear.addEventListener("click", clearAll);
