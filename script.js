baa8392cae1a7ad7070d4b9d8635c19ca470d899
Ahmed Omar <ahmedomar@hey.com>
2022-02-16 14:21:22 +0000

    add button arrays, added query selectors

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 // variables
-
-let queue = [];
-let input = 0;
-
 const numButton = document.getElementsByClassName("num-button");
 const operatorButton = document.getElementsByClassName("operator-button");
-const display = getElementById("result");
-const clear = getElementById("AC");
+const display = document.querySelector("#result");
+const clear = document.querySelector("#AC");
 
-
+const keys = [
+  "×",
+  "÷",
+  "+",
+  "-",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+];
+const operations = ["*", "÷", "+", "-"];
 
 // functions:
 
@@ -23,22 +36,20 @@ const displayFunc = () => {
   alert("it's working");
 };
 
-
 //Evaluate and give result
 const solve = () => {
   let x = document.getElementById("result").value;
   let y = document.getElementById("result").value;
 };
 
-
 //To clear the display
+
 const clearAll = () => {
   document.getElementById("result").value = "";
 };
 
-
-
 // logic
+
 for (let index = 0; index < numButton.length; index++) {
   numButton[index].addEventListener("click", displayFunc);
 }
@@ -46,10 +57,3 @@ for (let index = 0; index < numButton.length; index++) {
 operatorButton.addEventListener("click", solve);
 
 clear.addEventListener("click", clearAll);
-
-
-
-
-
-
-                       
