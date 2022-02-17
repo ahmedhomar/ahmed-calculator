80d920fb79b1bf3811df3dea448132fcad8f01a6
Ahmed Omar <ahmedomar@hey.com>
2022-02-17 14:44:08 +0000

    created click handler for number buttons

--- script.js
+++ script.js
@@ -10,8 +10,43 @@ const operatorButton = document.querySelectorAll(".operator-btn");// operator bu
 const display = document.getElementById("result");// input/output 
 const clear = document.getElementById("AC");// clear button
 const equalsBtn = document.getElementById("equals");// equals button
+outputDisplayed = false; //track what output is displayed
 
 let num;
+
+// click handlers for number buttons
+
+for (let i = 0; i < numButton.length; i++) {
+  numButton[i].addEventListener("click", displayFunc {
+// store current input string and its last character in variables 
+  const currentString = display.innerHTML;
+  const lastChar = currentString[currentString.length - 1];
+
+  // if result is not diplayed => keep adding
+
+    if (outputDisplayed === false) {
+      display.innerHTML += e.target.innerHTML;
+    } else if (outputDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+      // if result is currently displayed and user pressed an operator: keep adding to the string for next operation
+      outputDisplayed = false;
+      display.innerHTML += e.target.innerHTML;
+    } else {
+      // result is currently displayed & user pressed a number => clear input and add new input for new operation.
+      outputDisplayed = false;
+      display.innerHTML = "";
+      display.innerHTML += e.target.innerHTML;
+
+    }
+
+  });
+}
+
+
+
+
+
+
+
 
 const keys = [
   "×",
@@ -33,7 +68,7 @@ const keys = [
 ];
 const operations = ["×", "÷", "+", "-"];
 
-console.log(numButton);
+
 
 // functions:
 
@@ -65,9 +100,7 @@ const clearAll = () => {
 
 // logic
 
-for (let index = 0; index < numButton.length; index++) {
-  numButton[index].addEventListener("click", displayFunc);
-}
+
 
 // operatorButton.addEventListener("click", solve);
 
