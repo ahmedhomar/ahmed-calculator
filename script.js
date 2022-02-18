3a4c8d607d4e67c0321061d2a4cf4faaa43a0d25
Ahmed Omar <ahmedomar@hey.com>
2022-02-18 10:50:18 +0000

    fixed number button and operator even handlers: calculator can do operations with single diigits without decimals

--- script.js
+++ script.js
@@ -10,8 +10,11 @@ const operatorButton = document.querySelectorAll(".operator-btn"); // operator b
 const display = document.getElementById("result"); // input/output
 const clear = document.getElementById("AC"); // clear button
 const equalsBtn = document.getElementById("equals"); // equals button
+const decimalButton = document.querySelectorAll(".decimal"); // decimal button
 
-
+let currentString;
+let operatorString;
+let secondNumber;
 // Functions
 
 // click handlers for number buttons
@@ -19,146 +22,142 @@ const equalsBtn = document.getElementById("equals"); // equals button
 for (let i = 0; i < numButton.length; i++) {
   numButton[i].addEventListener("click", (e) => {
     // store current input string and its last character in variables
-    const currentString = display.innerHTML;
+    // check to see if display is empty - if it is then set currentString to displayValue, else chain numbers
+    currentString = e.target.value;
+    display.innerHTML = currentString;
     const lastChar = currentString[currentString.length - 1];
-   
-    console.log("working")
+    console.log(currentString);
 
-    //  if (lastChar === "+" ||
-    //   lastChar === "-" ||
-    //   lastChar === "×" ||
-    //   lastChar === "÷" )
-    //  {
-    //  display.innerHTML = e.target.innerHTML;
-    //    // if the last character is an operator
-
-    // } else {
-    //   // display input
-     
-      
-    //   display.innerHTML += e.target.innerHTML;
-    // }
-    
+    // if last character entered is an operator => replace it with the currently pressed one
   });
 }
 
-for (let i = 0; i < numButton.length; i++) {
-  numButton[i].addEventListener("click", (e) => {
-
+for (let i = 0; i < operatorButton.length; i++) {
+  operatorButton[i].addEventListener("click", (e) => {
     // store current input string and its last character in variables
-  let currentString = e.target.value;
-  display.innerHTML = currentString;
-  const lastChar = currentString[currentString.length - 1];
-  console.log(lastChar, currentString);
-  // if last character entered is an operator => replace it with the currently pressed one
+    operatorString = e.target.value;
+    const lastChar = operatorString[operatorString.length - 1];
+    secondNumber = currentString;
+    currentString = "";
 
-    if (lastChar === "+" ||lastChar === "-" ||lastChar === "×" ||lastChar === "÷") {
-      
-      //Take the current string, remove the last character and add the new character.
-      
-      const newString = currentString.substring(0, currentString.length - 1) + e.target.value;
-      display.innerHTML = newString;
-    } else if (currentString.length == 0) {
-      // if first button pressed is an operator => do nothing.
-     console.log("Please enter a number");
-    } else {
-      // else add the operator pressed to the input
-
-      display.innerHTML += e.target.innerHTML;
-    }
-    
-  })
+    // if last character entered is an operator => replace it with the currently pressed one
+  });
 }
 
-// Event Listener for "=" button:
- equalsBtn.addEventListener("click", (e) => {
+decimalButton.addEventListener("click", (e) =>
 
+
+
+
+
+
+// if (lastChar === "+" ||lastChar === "-" ||lastChar === "×" ||lastChar === "÷") {
+
+//   //Take the current string, remove the last character and add the new character.
+
+//   const newString = currentString.substring(0, currentString.length - 1) + e.target.value;
+//   display.innerHTML = newString;
+// } else if (currentString.length == 0) {
+//   // if first button pressed is an operator => do nothing.
+//  console.log("Please enter a number");
+// } else {
+//   // else add the operator pressed to the input
+
+//   display.innerHTML += e.target.value;
+// }
+
+// Event Listener for "=" button:
+equalsBtn.addEventListener("click", (e) => {
   // Display string we'll be processing: e.g 5+33-12/10
-  
-  const inputString = display.innerHTML;
+  console.log(currentString, secondNumber, operatorString);
+  // const inputString = display.innerHTML;
+  let sum = 0;
+  if (operatorString === "+") {
+    sum = parseFloat(currentString) + parseFloat(secondNumber);
+    console.log(sum);
+  } else if (operatorString === "-") {
+    sum = parseFloat(secondNumber) - parseFloat(currentString);
+    console.log(sum);
+  } else if (operatorString === "×") {
+    sum = parseFloat(secondNumber) * parseFloat(currentString);
+    console.log(sum);
+  } else if (operatorString === "÷") {
+    sum = parseFloat(secondNumber) / parseFloat(currentString);
+    console.log(sum);
+  } else {
+    display.innerHTML = "ERROR";
+  }
+  console.log(sum, currentString, secondNumber);
+  currentString = sum;
+  display.innerHTML = sum;
 
   //Split the input string into an array of numbers: ["5", "33", "12", "10"]
+});
+// const numbers = inputString.split(/\+|\-|\×|\÷/g); //It splits the input string into an array of numbers.
 
-  const numbers = inputString.split(/\+|\-|\×|\÷/g); //It splits the input string into an array of numbers.
+// // Create an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
+// // Replace all the numbers and dot with empty string and then split
 
-  // Create an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
-  // Replace all the numbers and dot with empty string and then split
-  
-  const operators = inputString.replace(/[0-9]|\./g, "").split(""); //It removes all numbers and decimals from the input string and splits the remaining characters into an array.
-  
-  console.log(inputString);
-  console.log(operators);
-  console.log(numbers);
-  console.log("----------------------------");
+// const operators = inputString.replace(/[0-9]|\./g, "").split(""); //It removes all numbers and decimals from the input string and splits the remaining characters into an array.
 
-  // now we need to loop through the array and doing one operation at a time.
-  // /, then *, then - and then + 
-  // the final element remaining in the array will be the output
+// console.log(inputString);
+// console.log(operators);
+// console.log(numbers);
+// console.log("----------------------------");
 
-  let divide = operators.indexOf("÷");
-  while (divide != -1) {
-    numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
-    operators.splice(divide, 1);
-    divide = operators.indexOf("÷");
-  }
- /*
+// now we need to loop through the array and doing one operation at a time.
+// /, then *, then - and then +
+// the final element remaining in the array will be the output
+
+// let divide = operators.indexOf("÷");
+// while (divide != -1) {
+//   numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+//   operators.splice(divide, 1);
+//   divide = operators.indexOf("÷");
+// }
+
+/*
   1. Loop runs as long as the index of the operator “÷” is not -1.
   2. The splice method removes the two numbers at the index of the operator “÷” and replace them with the result of the division.
   3. The operators array will be updated to remove the operator “÷”.
   4. The divide variable will be updated to the index of the next operator in the operators array.
   */
-  let multiply = operators.indexOf("×");
-  while (multiply != -1) {
-    numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
-    operators.splice(multiply, 1);
-    multiply = operators.indexOf("×");
-  }
+//   let multiply = operators.indexOf("×");
+//   while (multiply != -1) {
+//     numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+//     operators.splice(multiply, 1);
+//     multiply = operators.indexOf("×");
+//   }
 
- let subtract = operators.indexOf("-");
-  while (subtract != -1) {
-    numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
-    operators.splice(subtract, 1);
-    subtract = operators.indexOf("-");
-  }
-  let add = operators.indexOf("+");
-  while (add != -1) {
-    // parseFloat => parses a string into a floating point number: used here to avoid string concatenation
-    numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
-    operators.splice(add, 1);
-    add = operators.indexOf("+");
-  }
+//   let subtract = operators.indexOf("-");
+//   while (subtract != -1) {
+//     numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+//     operators.splice(subtract, 1);
+//     subtract = operators.indexOf("-");
+//   }
+//   let add = operators.indexOf("+");
+//   while (add != -1) {
+//     // parseFloat => parses a string into a floating point number: used here to avoid string concatenation
+//     numbers.splice(
+//       add,
+//       2,
+//       parseFloat(numbers[add]) + parseFloat(numbers[add + 1])
+//     );
+//     operators.splice(add, 1);
+//     add = operators.indexOf("+");
+//   }
 
-  display.innerHTML = numbers[0]; // displays the output
+//   display.innerHTML = numbers[0]; // displays the output
+// });
 
-  
- });
-
-
- // Pressing AC...
+// Pressing AC...
 
 clear.addEventListener("click", (e) => {
   display.innerHTML = "";
+  currentString = "";
+  operatorString = "";
+  secondNumber = "";
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const keys = [
 //   "×",
